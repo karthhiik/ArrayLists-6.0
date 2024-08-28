3a32c8dace6e16cdabc7cb8509d9bf53ffe1fa4e
karthhiik <karthikthatikonda18@gmail.com>
2024-08-28 18:25:45 +0530

    Update ForL.java

--- ForL.java
+++ ForL.java
@@ -1,7 +1,16 @@
-public class ForL{
+import java.util.*;
+public class Main{
 public static void main(String[]args){
 // prepare an object for ArrayList
   // Load String instead of Integers 
   // Iterate them using for each loop
+  ArrayList<String> food = new ArrayList<String>();
+  food.add("pizza");
+  food.add("burger");
+  food.add("biryani");
+  for(int i=0; i<food.size();i++){
+    System.out.println(food.get(i));
+  }     
+ 
 }
 }
